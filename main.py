1ebd4d42f54db2c2e4b6a89c4a152527b83e2884
Andrey <hamanist@gmail.com>
2023-03-12 01:11:30 +0300

    file update

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from func_json import data_to_json
+from function import data_to_json, filter_data, last_executed, data_formatting
+
+LATEST_VALUES = 5
+FILTER_FROM = True
 
 
 def main():
     data = data_to_json()
-    return len(data['id'])
-
+    data = filter_data(data, FILTER_FROM)
+    data = last_executed(data, LATEST_VALUES)
+    data = data_formatting(data)
+    for result in data:
+        print(result)
 
 
 if __name__ == '__main__':
